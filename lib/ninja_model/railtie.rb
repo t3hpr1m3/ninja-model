14fffbc1137f41b4994db841327a48a8741680f5
Josh Williams <jwilliams@shareone.com>
2011-05-15 22:47:48 -0500

    Renamed initializers.  Version bump to 0.5.1

--- lib/ninja_model/railtie.rb
+++ lib/ninja_model/railtie.rb
@@ -10,20 +10,18 @@ module NinjaModel
       NinjaModel::set_logger(Rails.logger)
     end
 
-    initializer 'ninja_model.extend_active_record' do |app|
+    initializer 'ninja_model_extend_active_record' do |app|
       ActiveSupport.on_load(:active_record) do
         require 'ninja_model/rails_ext/active_record'
       end
     end
 
-    initializer 'ninja_model.load_specs' do |app|
+    initializer 'ninja_model_load_specs' do |app|
       config_path = File.join(app.paths.config.to_a.first, "ninja_model.yml")
       if File.exists?(config_path)
         require 'erb'
         require 'yaml'
         app.config.ninja_model.specs = YAML::load(ERB.new(IO.read(config_path)).result)
-        puts "config: #{app.config.ninja_model.inspect}"
-        #NinjaModel::Base.set_adapter
       else
         NinjaModel.logger.warn "[ninja-model] *WARNING* Unable to find configuration file at #{config_path}"
       end
--- lib/ninja_model/version.rb
+++ lib/ninja_model/version.rb
@@ -1,3 +1,3 @@
 module NinjaModel
- VERSION = "0.5.0"
+ VERSION = "0.5.1"
 end
