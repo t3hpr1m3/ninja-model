9ad54f05e7512cde78e14683c8a2fb66a4449ad9
Josh Williams <jwilliams@shareone.com>
2013-08-08 16:56:00 -0500

    Cleaned up marshalling code

--- lib/ninja_model/marshalling.rb
+++ lib/ninja_model/marshalling.rb
@@ -4,11 +4,34 @@ module NinjaModel
 
     module InstanceMethods
       def marshal_dump
-        ActiveSupport::JSON.encode(attributes)
+        h = self.instance_variables.inject({}) { |r, k|
+          if k.to_s.eql?('@attributes')
+            r[:attributes] = @attributes.inject({}) { |a, (k, v)|
+              a[k] = ActiveSupport::JSON.encode(v)
+              a
+            }
+          else
+            r[k.to_s] = instance_variable_get(k)
+          end
+          r
+        }
+        h[:attributes] = {}
+        @attributes.each do |k, v|
+          h[:attributes][k] = ActiveSupport::JSON.encode(v)
+        end
+        ActiveSupport::JSON.encode(h)
       end
 
       def marshal_load(data)
-        self.instantiate(ActiveSupport::JSON.decode(data))
+        h = ActiveSupport::JSON.decode(data)
+        @attributes = {}
+        h.each do |k, v|
+          if k.eql?(:attributes)
+            @attributes[k] = ActiveSupport::JSON.decode(v)
+          else
+            instance_variable_set(k, v)
+          end
+        end
       end
     end
   end
